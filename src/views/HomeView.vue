fd1cc9d1653ca332e0450c7cb3c21df1cf91918d
Vitaliy Nifontov <vitaliynif@gmail.com>
2025-10-07 16:16:41 +0700

    feat: add headers, add plus icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,7 @@ import {
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
 import { computed, ref } from "vue";
+import { Plus } from "@element-plus/icons-vue";
 
 const userStore = useUserStore();
 
@@ -29,22 +30,24 @@ const removeUserFromStore = (user: IUserWithStringMark) => {
 
 const userToShow = computed(() => [
   ...users.value.map((user) => ({ ...user, mark: user.mark?.join(";") })),
-  ...newUsers.value
+  ...newUsers.value,
 ]);
 
 const uploadUser = (user: IUserWithStringMark) => {
   if (!user.login) return;
 
   const preparedUser = prepareUserToUpload(user);
-  
+
   // Remove from newUsers if it exists there
-  const newUserIndex = newUsers.value.findIndex(u => u.login === user.login);
+  const newUserIndex = newUsers.value.findIndex((u) => u.login === user.login);
   if (newUserIndex >= 0) {
     newUsers.value.splice(newUserIndex, 1);
   }
-  
+
   // Add or update in store
-  const existingIndex = users.value.findIndex(u => u.login === preparedUser.login);
+  const existingIndex = users.value.findIndex(
+    (u) => u.login === preparedUser.login
+  );
   if (existingIndex >= 0) {
     users.value[existingIndex] = preparedUser;
   } else {
@@ -64,10 +67,17 @@ const addBlankUser = () => {
 
 <template>
   <main>
-    <div>
-      <el-button @click="addBlankUser">Add user</el-button>
+    <div class="main-page__actions">
+      <el-button @click="addBlankUser"><el-icon><Plus /></el-icon></el-button>
     </div>
     <div class="main-page__users">
+      <div class="headers">
+        <div class="header-item">Метки</div>
+        <div class="header-item">Тип записи</div>
+        <div class="header-item">Логин</div>
+        <div class="header-item">Пароль</div>
+        <div class="header-spacer"></div>
+      </div>
       <app-user
         v-for="(user, index) in userToShow"
         :key="index"
@@ -76,14 +86,44 @@ const addBlankUser = () => {
         @remove-user="() => removeUserFromStore(user)"
       />
     </div>
-
   </main>
 </template>
 
 <style lang="scss">
+.main-page__actions {
+  margin: 2rem 0;
+}
+
 .main-page__users {
   display: flex;
   flex-direction: column;
   gap: 1rem;
+
+  .headers {
+    display: flex;
+    gap: 0.4rem;
+    align-items: center;
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+
+    .header-item {
+      &:nth-child(1) {
+        width: 150px;
+      } // Метки
+      &:nth-child(2) {
+        width: 120px;
+      } // Тип записи
+      &:nth-child(3) {
+        width: 150px;
+      } // Логин
+      &:nth-child(4) {
+        width: 150px;
+      } // Пароль
+    }
+
+    .header-spacer {
+      width: 24px; // Space for delete icon
+    }
+  }
 }
 </style>
